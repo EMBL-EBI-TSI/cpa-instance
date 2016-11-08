c0801c12be8c8539e5ad04e0c78ad1ae5424e152
Gianni Dalla Torre <giannidallatorre@gmail.com>
2016-11-08 10:46:56 +0000

    Update destroy.sh

--- gcp/destroy.sh
+++ gcp/destroy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
 # Destroys a virtual machine instance in GCP
+export TF_VAR_name="$(awk -v var="$PORTAL_DEPLOYMENT_REFERENCE" 'BEGIN {print tolower(var)}')"
+
 terraform destroy --force --state=$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/terraform.tfstate' $PORTAL_APP_REPO_FOLDER'/gcp/terraform'
