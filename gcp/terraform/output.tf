9750d6f46c8f5c2bfe557a6d97322a8c1458b009
Gianni Dalla Torre <gianni@ebi.ac.uk>
2016-11-09 10:20:35 +0000

    Fix assignment of external_ip output variable

--- gcp/terraform/output.tf
+++ gcp/terraform/output.tf
@@ -1,3 +1,3 @@
 output "external_ip" {
-    value = "${google_compute_instance.instance.ip_address}"
+    value = "${google_compute_instance.instance.network_interface.0.access_config.0.assigned_nat_ip}"
 }
