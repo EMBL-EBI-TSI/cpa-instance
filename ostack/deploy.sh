b9d81b409b07768e74839b712bda2e2363712ca5
Gianni Dalla Torre <gianni@ebi.ac.uk>
2017-08-14 16:10:32 +0100

    Add portal scripts for maintain the VM lifecyle

---
+++ ostack/deploy.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# Set color variable
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+set -e
+# Provisions a virtual machine instance
+
+# Local variables
+export APP="${PORTAL_APP_REPO_FOLDER}"
+echo "export APP=${APP}"
+
+export TF_VAR_deployment_path="${PORTAL_DEPLOYMENTS_ROOT}/${PORTAL_DEPLOYMENT_REFERENCE}"
+echo "export TF_VAR_deployment_path=${TF_VAR_deployment_path}"
+
+export DPL="${PORTAL_DEPLOYMENTS_ROOT}/${PORTAL_DEPLOYMENT_REFERENCE}/"
+echo "export DPL=${DPL}"
+
+export PRIV_KEY_PATH="${DPL}${PORTAL_DEPLOYMENT_REFERENCE}"
+echo "export PRIV_KEY_PATH=${PRIV_KEY_PATH}"
+
+# Export input variables in the bash environment
+export TF_VAR_name="$(awk -v var="${PORTAL_DEPLOYMENT_REFERENCE}" 'BEGIN {print tolower(var)}')"
+echo "export TF_VAR_name=${TF_VAR_name}"
+
+export KEY_PATH="${DPL}${PORTAL_DEPLOYMENT_REFERENCE}.pub"
+echo "export KEY_PATH=${KEY_PATH}"
+
+export TF_VAR_key_path="${KEY_PATH}"
+echo "export TF_VAR_key_path=${TF_VAR_key_path}"
+
+export TF_STATE=${DPL}'terraform.tfstate'
+echo "export TF_STATE=${TF_STATE}"
+
+# Launch provisioning of the VM
+echo -e "\n\t${CYAN}Terraform apply${NC}\n"
+terraform apply --state=${DPL}'terraform.tfstate' ${APP}'/ostack/terraform'
+
+# Print the ssh configuration file
+cat "${DPL}ssh.config"
+
+# Extract the external IP of the instance
+external_ip=$(terraform output -state=${DPL}'terraform.tfstate' external_ip)
+
+# Launch Ansible playbook
+cd ostack/ansible || exit
+echo -e "\n\t${CYAN}Launch Ansible playbook${NC}\n"
+ansible-playbook -b cluster.yml
