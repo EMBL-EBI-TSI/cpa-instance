f7e9ef0029315b021583000e0d22ed582b18fabd
gef <gef@ebi.ac.uk>
2021-10-28 23:13:27 +0100

    Minimal modification

--- ostack/state.sh
+++ ostack/state.sh
@@ -3,5 +3,6 @@ set -e
 # Get the status of a virtual machine instance
 
 # Query Terraform state file
-terraform show $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/terraform.tfstate'
-
+export APP="${PORTAL_APP_REPO_FOLDER}"
+export DPL="${PORTAL_DEPLOYMENTS_ROOT}/${PORTAL_DEPLOYMENT_REFERENCE}/"
+terraform -chdir=${APP}'/ostack/terraform'  show -json ${DPL}'terraform.tfstate'
--- ostack/terraform/variable.tf
+++ ostack/terraform/variable.tf
@@ -8,25 +8,51 @@ variable "instances" {
   description = "Number of instances to deploy"
 }
 
+variable "public_key_path" {
+  description = "The path of the public SSH key to be injected in the vm"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
 variable "disk_image_name" {
-  default     = "ubuntu-16.04"
+  default     = "ubuntu-16_04"
   description = "OS image name to use for node installation"
 }
 
 variable "machine_type" {
-  default     = "s1.nano"
+  default     = "1c2m20d"
   description = "Machine type (flavor)"
+}
+
+variable "instance_volume_size" {
+  default     = "20"
+  description = "Instance volume size."
 }
 
 variable "network_name" {
   description = "The name of the OpenStack network where to deploy the instance"
 }
 
-variable "profile_public_key" {
-  description = "The public SSH key to be injected in the vm"
-  type        = "string"
-}
-
 variable "floating_ip_pool" {
   default = "net_external"
+}
+
+variable "user_name" {
+  default = "default.user_name"
+  description = "Your email (although I don't think the value is used or verified at any point!)"
+}
+
+variable "tenant_name" {
+  default = "default.tenant_name"
+  description = "See https://uk1.embassy.ebi.ac.uk/identity/application_credentials/create/, or OpenStack CLI openstack project list"
+}
+
+variable "region" {
+  default = "RegionOne"
+  description = "See https://uk1.embassy.ebi.ac.uk/identity/application_credentials/create/ or OpenStack CLI openstack region list"
+}
+
+variable "auth_url" {
+  default = "https://uk1.embassy.ebi.ac.uk:5000"
+  description = "See https://uk1.embassy.ebi.ac.uk/project/api_access/ (Identity Service), or https://uk1.embassy.ebi.ac.uk/identity/application_credentials/create/"
 }
